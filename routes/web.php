e63a6f7c74ad35622a0e6b8105b95440252d56cc
Iman Maliki <iman@dne.co.id>
2023-06-20 21:33:34 +0700

    test laravel/ui

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'Controller@index');
 Route::get('/artisan', 'Controller@artisan');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
