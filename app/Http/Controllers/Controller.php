13e8eb3b787e6d06e3d4e9761dd564386754495e
Iman Maliki <iman@dne.co.id>
2023-06-20 16:45:14 +0700

    initial commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Artisan;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function artisan(Request $request) {
+        if(isset($request->call)) {
+            Artisan::call($request->call);
+            return "Artisan call " . $request->call;
+        }
+    }
+
+    public function index() {
+        return view('welcome');
+    }
+}
